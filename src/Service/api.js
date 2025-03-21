ba79977735cb18ea2403006f13acb144d1453efc
SPECINDIA\shraddha.parmar <shraddha.parmar@spec-india.com>
2025-03-21 14:56:18 +0530

    change backend URL

--- src/Service/api.js
+++ src/Service/api.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 //const axios = require("axios"); // Fixing "Cannot use import statement outside a module" in GitHub Actions
-const API_URL = "https://localhost:7085/api/Employee"; // Adjust based on your .NET API URL
+//const API_URL = "https://localhost:7085/api/Employee"; // Adjust based on your .NET API URL
+const API_URL = "https://azureservicedemoapi-e2e4apfsedajbwb7.canadacentral-01.azurewebsites.net/api/Employee"; // Adjust based on your .NET API URL
 
 export const getEmployees = async () => {
   try {
